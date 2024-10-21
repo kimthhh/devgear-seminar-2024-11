08d38cb5ac7238cc73b039be192ab5ebd7b335cb
PointHubKr <pointhubkr@gmail.com>
2024-10-21 22:28:29 +0900

    # Add: (세미나용) ApiServer > Redis + XSuperObject Lib 프로젝트에 등록

--- API-SERVER/ApiServer.dpr
+++ API-SERVER/ApiServer.dpr
@@ -6,7 +6,19 @@ uses
   uSrvMAIN in 'uSrvMAIN.pas' {fSvrMAIN},
   uLogWriteThread in 'uLogWriteThread.pas',
   CommonService in 'CommonService.pas',
-  CommonServiceImplementation in 'CommonServiceImplementation.pas';
+  CommonServiceImplementation in 'CommonServiceImplementation.pas',
+  uPrsREDIS in 'RedisModule\uPrsREDIS.pas',
+  JsonDataObjects in 'RedisModule\delphiredisclient\sources\JsonDataObjects.pas',
+  Redis.Client in 'RedisModule\delphiredisclient\sources\Redis.Client.pas',
+  Redis.Command in 'RedisModule\delphiredisclient\sources\Redis.Command.pas',
+  Redis.Commons in 'RedisModule\delphiredisclient\sources\Redis.Commons.pas',
+  Redis.NetLib.Factory in 'RedisModule\delphiredisclient\sources\Redis.NetLib.Factory.pas',
+  Redis.NetLib.INDY in 'RedisModule\delphiredisclient\sources\Redis.NetLib.INDY.pas',
+  Redis.Values in 'RedisModule\delphiredisclient\sources\Redis.Values.pas',
+  RedisMQ.Commands in 'RedisModule\delphiredisclient\sources\RedisMQ.Commands.pas',
+  RedisMQ in 'RedisModule\delphiredisclient\sources\RedisMQ.pas',
+  XSuperJSON in 'XSuperObject\XSuperJSON.pas',
+  XSuperObject in 'XSuperObject\XSuperObject.pas';
 
 {$R *.res}
 
